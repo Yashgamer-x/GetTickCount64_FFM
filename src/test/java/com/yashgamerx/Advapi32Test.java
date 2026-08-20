2ffdadae1c46bc28c9bf19e7fc46a3ec70b0d127
Yashgamer-x <yashdesai121212@gmail.com>
2026-08-20 04:57:51 -0400

    Created test case for Advapi32.getUserNameW

---
+++ src/test/java/com/yashgamerx/Advapi32Test.java
@@ -0,0 +1,23 @@
+package com.yashgamerx;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.yashgamerx.Advapi32;
+
+import java.lang.foreign.Arena;
+
+public class Advapi32Test {
+
+    @Test
+    public void testGetUsernameW() {
+        try(Arena arena = Arena.ofConfined()) {
+            Advapi32 advapi32 = new Advapi32(arena);
+            var username = advapi32.getUserNameW();
+            Assertions.assertNotNull(username);
+            System.out.println(username);
+        } catch (Throwable e) {
+            Assertions.fail();
+        }
+    }
+
+}
