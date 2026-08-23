3c31b594b5bf2029d6d281ec6d757f357bcf8dec
Yashgamer-x <yashdesai121212@gmail.com>
2026-08-23 08:01:36 -0400

    Refactored `getNativeSystemInfo` in Kernel32 to use new dedicated `GetNativeSystemInfo` class; added `GetNativeSystemInfo` implementation for method encapsulation and improved code structure.

--- src/main/java/org/yashgamerx/kernel/Kernel32.java
+++ src/main/java/org/yashgamerx/kernel/Kernel32.java
@@ -5,6 +5,7 @@ import org.yashgamerx.kernel.oem.ProcessorArchitecture;
 import org.yashgamerx.kernel.oem.System_Info;
 import org.yashgamerx.kernel.fileapi.GetFileAttributeW;
 import org.yashgamerx.kernel.fileapi.GetLogicalDrives;
+import org.yashgamerx.kernel.sysinfoapi.GetNativeSystemInfo;
 import org.yashgamerx.kernel.sysinfoapi.GetSystemInfo;
 import org.yashgamerx.kernel.sysinfoapi.GetTickCount64;
 
@@ -20,7 +21,7 @@ public class Kernel32 {
     private final GetFileAttributeW getFileAttributeW;
     private final GetLogicalDrives getLogicalDrives;
     private final GetSystemInfo getSystemInfo;
-    private final MethodHandle getNativeSystemInfo;
+    private final GetNativeSystemInfo getNativeSystemInfo;
     private final MethodHandle getCurrentProcess;
     private final MethodHandle getProcessId;
     private final MethodHandle getProcessTimes;
@@ -37,21 +38,12 @@ public class Kernel32 {
         getFileAttributeW = new GetFileAttributeW(arena, kernel32);
         getLogicalDrives = new GetLogicalDrives(kernel32);
         getSystemInfo = new GetSystemInfo(arena, kernel32);
-        getNativeSystemInfo = createGetNativeSystemInfo();
+        getNativeSystemInfo = new GetNativeSystemInfo(arena, kernel32);
         getCurrentProcess = createGetCurrentProcess();
         getProcessId = createGetProcessId();
         getProcessTimes = createGetProcessTimes();
         fileTimeToSystemTime = createFileTimeToSystemTime();
         getSystemTimeAsFileTime = createGetSystemTimeAsFileTime();
-    }
-
-    private MethodHandle createGetNativeSystemInfo() {
-        MemorySegment getSystemInfo_addr = kernel32.find("GetNativeSystemInfo")
-                .orElseThrow();
-        return linker.downcallHandle(
-                getSystemInfo_addr,
-                FunctionDescriptor.ofVoid(ValueLayout.ADDRESS)
-        );
     }
 
     private MethodHandle createGetCurrentProcess() {
@@ -127,51 +119,7 @@ public class Kernel32 {
     }
 
     public System_Info getNativeSystemInfo() throws Throwable {
-        MemoryLayout SYSTEM_INFO_LAYOUT = System_Info.SYSTEM_INFO_LAYOUT;
-        MemorySegment SYSTEM_INFO_SEGMENT = arena.allocate(SYSTEM_INFO_LAYOUT);
-        getNativeSystemInfo.invokeExact(SYSTEM_INFO_SEGMENT);
-
-        int dwOemId = (int) System_Info.DW_OEM_ID_VAR_HANDLE.get(SYSTEM_INFO_SEGMENT, 0L);
-        short wProcessorArchitecture = (short) System_Info.W_PROCESSOR_ARCHITECTURE_VAR_HANDLE.get(SYSTEM_INFO_SEGMENT, 0L);
-        short wReserved = (short) System_Info.W_RESERVED_VAR_HANDLE.get(SYSTEM_INFO_SEGMENT, 0L);
-        int dwPageSize = (int) System_Info.DW_PAGE_SIZE_VAR_HANDLE.get(SYSTEM_INFO_SEGMENT, 0L);
-        MemorySegment lpMinimumApplicationAddress =
-                (MemorySegment) System_Info.LP_MINIMUM_APPLICATION_ADDRESS_VAR_HANDLE.get(SYSTEM_INFO_SEGMENT, 0L);
-        MemorySegment lpMaximumApplicationAddress =
-                (MemorySegment) System_Info.LP_MAXIMUM_APPLICATION_ADDRESS_VAR_HANDLE.get(SYSTEM_INFO_SEGMENT, 0L);
-        long dwActiveProcessorMask = (long) System_Info.DW_ACTIVE_PROCESSOR_MASK_VAR_HANDLE.get(SYSTEM_INFO_SEGMENT, 0L);
-        int dwNumberOfProcessors = (int) System_Info.DW_NUMBER_OF_PROCESSORS_VAR_HANDLE.get(SYSTEM_INFO_SEGMENT, 0L);
-        int dwProcessorType = (int) System_Info.DW_PROCESSOR_TYPE_VAR_HANDLE.get(SYSTEM_INFO_SEGMENT, 0L);
-        int dwAllocationGranularity = (int) System_Info.DW_ALLOCATION_GRANULARITY_VAR_HANDLE.get(SYSTEM_INFO_SEGMENT, 0L);
-        short wProcessorLevel = (short) System_Info.W_PROCESSOR_LEVEL_VAR_HANDLE.get(SYSTEM_INFO_SEGMENT, 0L);
-        short wProcessorRevision = (short) System_Info.W_PROCESSOR_REVISION_VAR_HANDLE.get(SYSTEM_INFO_SEGMENT, 0L);
-
-        OemId oemId = getOemId(dwOemId, wProcessorArchitecture, wReserved);
-
-        return new System_Info(
-                oemId,
-                dwPageSize,
-                lpMinimumApplicationAddress,
-                lpMaximumApplicationAddress,
-                dwActiveProcessorMask,
-                dwNumberOfProcessors,
-                dwProcessorType,
-                dwAllocationGranularity,
-                wProcessorLevel,
-                wProcessorRevision
-        );
-    }
-
-    private static OemId getOemId(int dwOemId, short wProcessorArchitecture, short wReserved) {
-        return switch (Short.toUnsignedInt(wProcessorArchitecture)) {
-                case 0, 5, 6, 9, 12, 0xFFFF ->
-                        new ProcessorArchitecture(wProcessorArchitecture, wReserved);
-
-                default ->
-                        throw new IllegalStateException(
-                                "Unknown processor architecture: " +Short.toUnsignedInt(wProcessorArchitecture)
-                        );
-            };
+        return getNativeSystemInfo.invoke();
     }
 
     public MemorySegment getCurrentProcess() throws Throwable {
