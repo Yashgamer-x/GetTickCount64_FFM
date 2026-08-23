56fd7b7877e299a769487bd50e9046413bc96cde
Yashgamer-x <yashdesai121212@gmail.com>
2026-08-23 09:23:39 -0400

    Refactored test package structures in `Kernel32Test` and `Advapi32Test` to improve organization and align with namespace conventions.

--- src/test/java/com/yashgamerx/advapi/Advapi32Test.java
+++ src/test/java/com/yashgamerx/advapi/Advapi32Test.java
@@ -1,4 +1,4 @@
-package com.yashgamerx;
+package com.yashgamerx.advapi;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
--- src/test/java/com/yashgamerx/kernel/Kernel32Test.java
+++ src/test/java/com/yashgamerx/kernel/Kernel32Test.java
@@ -1,4 +1,4 @@
-package com.yashgamerx;
+package com.yashgamerx.kernel;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
